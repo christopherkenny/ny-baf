83c9e0d25a4061b89cc18533646fdb495678a150
Christopher Kenny <ctk44@cornell.edu>
2024-02-27 18:42:02 -0500

    Update A09310.R
    nys
    
    add nys data

--- R/A09310.R
+++ R/A09310.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(here)
 library(fs)
+library(sf)
 
 # html processing
 library(rvest)
@@ -55,15 +56,16 @@ txt <- txt |>
     tract = str_detect(text, '^Tract: '),
     block = str_detect(text, 'Block: '),
     blocks = str_detect(text, 'Blocks: '),
-    county = case_when(
+    county_name = case_when(
       all_of_county ~ str_remove(text, 'All of '),
       within_county ~ str_remove(text, 'Within '),
       TRUE ~ NA_character_
     )
   ) |> 
   group_by(district) |> 
-  fill(county, .direction = 'down') |> 
-  ungroup()
+  fill(county_name, .direction = 'down') |> 
+  ungroup() |> 
+  mutate(district = as.integer(district))
 
 whole_county <- txt |> 
   filter(all_of_county)
@@ -73,8 +75,7 @@ whole_town <- txt |>
 
 remainder <- txt |> 
   filter(!all_of & !within) |> 
-  select(text, district, county, tracts:blocks)
-
+  select(text, district, county_name, tracts:blocks)
 
 
 # set up baf to patch into ----
@@ -89,10 +90,129 @@ county_lookup <- fips_2020 |>
 
 baf <- pl_get_baf(state, 'INCPLACE_CDP')[[1]]
 
-places <- tt_places(state)
-town_lookup <- places |> 
-  sf::st_drop_geometry() |> 
-  select(town_fips = GEOID, town_name = NAME)
+mcd <- pl_get_baf(state, 'MCD')[[1]] |> 
+  mutate(
+    block_fips = BLOCKID,
+    mcd_fips = paste0(str_sub(BLOCKID, 1, 2), COUNTYFP, COUSUBFP),
+    .keep = 'none'
+  )
 
-target <- baf
+cousub <- tt_county_subdivisions(state)
+town_lookup <- cousub |>
+  st_drop_geometry() |>
+  select(town_fips = GEOID, town_name = NAMELSAD)
 
+target <- baf |> 
+  select(block_fips = BLOCKID) |> 
+  mutate(
+    state_fips = str_sub(block_fips, 1, 2),
+    county_fips = str_sub(block_fips, 1, 5),
+    tract_fips = str_sub(block_fips, 1, 11),
+    district = NA_integer_
+  )
+
+# fill in whole counties ----
+
+target <- whole_county |> 
+  select(county_name, district) |> 
+  left_join(county_lookup, by = 'county_name') |> 
+  select(-county_name) |> 
+  rows_patch(x = target, by = 'county_fips')
+
+cli::cli_inform('whole counties account for {round(100 * sum(!is.na(target$district)) / nrow(target), 1)}% of blocks')
+
+# fill in whole towns ----
+
+whole_town_fips <- whole_town |> 
+  mutate(
+    town_name = str_remove(text, 'All of '),
+    town_name = str_replace(town_name, 'LaGrange', 'La Grange'),
+    town_name = str_replace(town_name, 'St. Johnsville', 'St Johnsville'),
+    town_name = str_squish(town_name)
+  ) |> 
+  select(town_name, county_name, district) |> 
+  left_join(county_lookup, by = 'county_name') |>
+  select(-county_name) |>
+  left_join(town_lookup |> mutate(county_fips = str_sub(town_fips, 1, 5)), by = c('town_name', 'county_fips'))
+
+town_baf <- mcd |> 
+  left_join(whole_town_fips, by = c('mcd_fips' = 'town_fips'), relationship = 'many-to-many') |>
+  filter(!is.na(district)) |> 
+  select(block_fips, district)
+
+target <- target |> 
+  rows_patch(town_baf, by = 'block_fips')
+
+cli::cli_inform('towns increase to {round(100 * sum(!is.na(target$district)) / nrow(target), 1)}% of blocks')
+
+# do the specified tracts and blocks ----
+
+whole_tracts <- remainder |> 
+  mutate(
+    text = str_remove(text, 'Tracts: '),
+    nchar_first_num = nchar(str_extract(text, '\\d+'))
+  ) |> 
+  filter(!tract, nchar_first_num == 6) |> 
+  mutate(
+    text = str_split(text, ' ')
+  ) |> 
+  unnest_longer(text)
+
+whole_tract <- remainder |> 
+  filter(tract, !blocks & !block) |> 
+  mutate(
+    text = str_remove(text, 'Tract: ')
+  )
+
+whole_tracts_all <- whole_tracts |> 
+  bind_rows(whole_tract) |> 
+  select(
+    county_name, tract_fips = text, district
+  ) |> 
+  left_join(county_lookup, by = 'county_name') |> 
+  select(-county_name) |> 
+  mutate(
+    tract_fips = paste0(county_fips, tract_fips)
+  )
+
+
+part_tracts <- remainder |> 
+  mutate(
+    nchar_first_num = nchar(str_extract(text, '\\d+'))
+  ) |> 
+  filter(tract | nchar_first_num == 4, blocks | block | nchar_first_num == 4) |> 
+  mutate(
+    tract_fips = str_extract(text, '\\d{6}'),
+    text = str_remove(text, 'Tract: \\d{6} Blocks: '),
+    text = str_remove(text, 'Tract: \\d{6} Block: ')
+  )  |> 
+  fill(tract_fips, .direction = 'down') |> 
+  mutate(
+    text = str_split(text, ' ')
+  ) |> 
+  unnest_longer(text) |> 
+  mutate(
+    text = str_replace(text, '-', ':')
+  ) |> 
+  rowwise() |> 
+  mutate(text = list(eval(parse(text = text)))) |> 
+  unnest_longer(text) |> 
+  mutate(
+    text = str_pad(text, 4, pad = '0')
+  ) |> 
+  select(
+    county_name, tract_fips, block_fips = text, district
+  ) |> 
+  left_join(county_lookup, by = 'county_name') |> 
+  select(-county_name) |> 
+  mutate(
+    tract_fips = paste0(county_fips, tract_fips),
+    block_fips = paste0(tract_fips, block_fips)
+  )
+
+target <- target |> 
+  rows_patch(whole_tracts_all, by = c('tract_fips')) |> 
+  rows_patch(part_tracts, by = c('block_fips'))
+
+
+cli::cli_inform('tracts increase to {round(100 * sum(!is.na(target$district)) / nrow(target), 1)}% of blocks')
